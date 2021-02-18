6541e7b08d38487103e745f81fc209f1415826d3
Vítor Holtz <vitorholtz27@gmail.com>
2021-02-18 11:56:38 -0300

    Update main.js

--- main.js
+++ main.js
@@ -76,7 +76,7 @@ window.onscroll = function () {
     let currentScrollPos = window.pageYOffset;
     let position1 = 1; 
     let position2 = 900; 
-    let position3 = 2400; 
+    let position3 = 2100; 
     let position4 = 2950; 
     let position5 = 4000;
 

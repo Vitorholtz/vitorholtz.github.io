b0d5f854fc4b82a83644b9332f2bf4978a90f27e
Vítor Holtz <vitorholtz27@gmail.com>
2021-02-18 11:09:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ window.onscroll = function () {
     let position1 = 1; 
     let position2 = 1100; 
     let position3 = 2100; 
-    let position4 = 3100; 
+    let position4 = 2900; 
     let position5 = 4000;
 
     if (currentScrollPos >= position2) {
